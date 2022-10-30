5e590c26a6ccad77f3d68358356894e7c83aa751
Arindam-7 <msaha6368@gmail.com>
2022-10-30 17:02:49 +0530

    cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import DatingCards from './components/DatingCards';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <DatingCards />
     </div>
   );
 }
