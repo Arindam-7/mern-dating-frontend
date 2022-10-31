b89ffa088b9cd4bad345fe518ca1bd2ee6bafede
Arindam-7 <msaha6368@gmail.com>
2022-10-31 16:52:11 +0530

    added buttons styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import DatingCards from './components/DatingCards';
+import SwipeButtons from './components/SwipeButtons'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <DatingCards />
+      <SwipeButtons />
     </div>
   );
 }
